8cc0d2f38d37298cfd6036a399a2e355d4de5321
frantic-dev <107622593+frantic-dev@users.noreply.github.com>
2022-09-02 16:00:35 +0100

    link sass file and add style

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
+import "../styles/style.scss";
+
 const body = document.querySelector('body');
 function htmlBody() {
     body.innerHTML = `
-        <button id="default-project" >DEFAULT PROJECT</button>
-        <button id="add-project">ADD A PROJECT +</button>
-        <br>
+        
+    <div id="all-projects">
+    <button id="add-project">ADD A PROJECT +</button>
+    <button id="default-project" >DEFAULT PROJECT</button>
+    </div>
+    <div id="all-todos">
         <input type="text"/>
         <button id="add-todo">&#10004;</button>
         <div id="display" class="project-default"></div>
+    </div>
 
     `
 }
